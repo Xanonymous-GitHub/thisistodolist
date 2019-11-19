b81989874d7b860b3f8c88b38dad51d0c2a45da5
Xanonymous <trusaidlin@gmail.com>
2019-11-19 12:33:44 +0800

    add alot of outerface

--- js/main.js
+++ js/main.js
@@ -1,93 +1,110 @@
 $(function () {
-    let divTemplate = (i_class, i_css, i_text) => {
-        let $item_div = $("<div\>");
+    var divTemplate = (i_class, i_text) => {
+        var $item_div = $("<div\>");
         $item_div.addClass(i_class);
-        $item_div.css(i_css);
         $item_div.text(i_text);
         return $item_div;
     };
-    $.fn.createNewItems = function (user_input, item_id, is_checked) {
+    $.fn.createNewItems = (user_input, item_id, is_checked) => {
         let $del_btn = $("<button\>");
-        let $chk_box = $("<input>");
-        $del_btn.text("Del");
-        $del_btn.addClass("btn btn-danger");
-        $del_btn.css("whiteSpace", "nowrap");
-        $del_btn.attr("name", item_id);
-        $del_btn.click(function (e) {
+        let $edit_btn = $("<button\>");
+        let $chk_box = $("<input\>");
+        $del_btn.click(() => {
             $.ajax({
                 type: "DELETE",
                 url: "/todolist/" + $(this).attr("name"),
-                beforeSend: function () {
+                beforeSend: () => {
 
                 },
-                complete: function () {
+                complete: () => {
 
                 },
-                fail: function (e) {
+                fail: () => {
 
                 },
-                success: function () {
-                    $del_btn.hide().parent().parent().parent().fadeOut(500, function () {
+                success: () => {
+                    $del_btn.parent().parent().parent().fadeOut(500, () => {
                         $del_btn.remove();
                     });
                 },
             });
-        });
-        $chk_box.attr({
+        }).text("刪除").addClass("btn-sm btn-danger").attr("name", item_id);
+        $chk_box.prop("checked", is_checked).attr({
             "type": "checkbox",
             "name": item_id
         });
-        $chk_box.prop("checked", is_checked);
-        $chk_box.click(function (e) {
-            let status = $(this).prop("checked");
-            $(this).prop("checked", !status);
+        $chk_box.click(() => {
             $.ajax({
                 type: "PUT",
-                url: "/todolist/" + $(this).attr("name"),
+                url: "/todolist/" + $(this).attr("name") + "/status",
                 data: JSON.stringify({ status: status }),
                 dataType: "JSON",
                 contentType: "application/json",
-                beforeSend: function () {
+                beforeSend: () => {
+                    let status = $(this).prop("checked");
+                    $(this).prop("checked", !status);
+                },
+                complete: () => {
 
                 },
-                complete: function () {
+                fail: () => {
 
                 },
-                fail: function (e) {
-                    
-                },
-                success: function () {
+                success: () => {
                     $chk_box.prop("checked", status);
                 },
             });
         });
-        let $new_item = divTemplate("mx-2 my-2 to_do_list_title", { "word-break": "break-all" });
-        let $new_item_container = divTemplate("row w-100");
-        let $content_container = divTemplate("col-10", {}, user_input);
-        let $button_container = divTemplate("col-2 p-0 text-right");
-        $button_container.append($del_btn);
-        $content_container.prepend($chk_box);
-        $new_item_container.append($content_container)
-        $new_item_container.append($button_container);
-        $new_item.append($new_item_container);
-        $(this).prepend($new_item.hide().fadeIn(600));
+        $edit_btn.click(() => {
+            //edit ajax
+        }).text("編輯").addClass("btn-sm btn-warning");
+        $("#to_do_list_container").prepend
+            (divTemplate("to_do_list_title mx-2 my-2 d-flex", "")
+                .append(
+                    divTemplate("input-group-prepend mx-0")
+                        .append(
+                            divTemplate("input-group-text").append($chk_box)
+                        )
+                )
+                .append(
+                    divTemplate("p-0 m-1")
+                        .append(
+                            divTemplate("p-0")
+                                .append($edit_btn)
+                                .append($del_btn)
+                                .css({ "display": "inline-block" })
+                        )
+                        .css({ "display": "inline-block" })
+                )
+                .append(
+                    divTemplate("flex-fill", user_input).css({ "word-break": "break-all", "display": "block" })
+                ));
     };
     $("body").css("background-color", "#9400D3");
-    $("#new_item").click(function (e) {
+    $("#new_item").click( (e)=> {
         let user_input = $("#input_area").val();
         if (user_input.trim() != "") {
-            $.post("/todolist", JSON.stringify({ user_input: user_input }), function (data) {
+            $("#input_area").val('');
+            $("#to_do_list_container").createNewItems(user_input, "000", false);
+            $.post("/todolist", JSON.stringify({ user_input: user_input }),  (data)=> {
                 $("#input_area").val('');
                 $("#to_do_list_container").createNewItems(user_input, data.item_id, false);
-            }, "JSON").fail(() => { });
+            }, "JSON").fail(() => { alert("Connection failed!"); });
         }
         else {
             alert("please input something!")
         }
     });
-    $.get("/todolist/lists", function (data) {
+    $("#input_area").keyup((e) => {
+        let code = (e.keyCode ? e.keyCode : e.which);
+        if (code === 13) {
+            e.preventDefault();
+            $("#new_item").click();
+        }
+    });
+    $.get("/todolist/lists",  (data)=> {
         for (let i = 0; i < data.length; i++) {
             $("#to_do_list_container").createNewItems(data[i].user_input, data[i].item_id, data[i].status);
         }
-    }).fail(() => { });
+    }).fail(() => { alert("Connection failed!"); });
 });
--- js/main_vue.js
+++ js/main_vue.js
@@ -9,4 +9,29 @@ let right = new Vue({
     data: {
         title: 'Xanonymous To-Do list'
     }
-})
+});
+Vue.component('todo-item',{
+    element:function(){
+
+    },
+    template: `<div class="to_do_list_title mx-2 my-2 d-flex"><div class="input-group-prepend mx-0"><div class="input-group-text"><input type="checkbox" aria-label="Checkbox for following text input"></div></div> <div class=" p-0 m-1" style="display: inline-block;"><div class="p-0" style="display: inline-block;"><button type="button" class="btn-sm btn-warning ">編輯</button></div> <div class="p-0" style="display: inline-block;"><button type="button" class="btn-sm btn-danger ">刪除</button></div></div> <div class="flex-fill" style="word-break: break-all; display: block;">{{}}</div></div>`,
+
+});
+let generate_new_item_block = new Vue({
+    el:'to_do_list_container',
+    data:{
+
+    },
+    computed:{
+        
+    },
+    method:{
+        dynamicComponent: function () {
+            return {
+                template: `<div class="to_do_list_title mx-2 my-2 d-flex"><div class="input-group-prepend mx-0"><div class="input-group-text"><input type="checkbox" aria-label="Checkbox for following text input"></div></div> <div class=" p-0 m-1" style="display: inline-block;"><div class="p-0" style="display: inline-block;"><button type="button" class="btn-sm btn-warning ">編輯</button></div> <div class="p-0" style="display: inline-block;"><button type="button" class="btn-sm btn-danger ">刪除</button></div></div> <div class="flex-fill" style="word-break: break-all; display: block;">{{}}</div></div>`,
+
+            }
+        }
+    },
+    //<div class="to_do_list_title mx-2 my-2 d-flex"><div class="input-group-prepend mx-0"><div class="input-group-text"><input type="checkbox" aria-label="Checkbox for following text input"></div></div> <div class=" p-0 m-1" style="display: inline-block;"><div class="p-0" style="display: inline-block;"><button type="button" class="btn-sm btn-warning ">編輯</button></div> <div class="p-0" style="display: inline-block;"><button type="button" class="btn-sm btn-danger ">刪除</button></div></div> <div class="flex-fill" style="word-break: break-all; display: block;">sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo</div></div>
+});
