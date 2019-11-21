fa0908614b0e9507dc915922d499bfc1565a5f54
Xanonymous <trusaidlin@gmail.com>
2019-11-21 17:47:39 +0800

    pdaopsdop

--- thisistodolist/src/App.vue
+++ thisistodolist/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <todolist title="This is To Do List"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import todolist from './components/todolist.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    todolist
   }
 }
 </script>
