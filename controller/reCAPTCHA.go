dbfe9aa9b00bffc5c8d14001911669cabbb55789
txya900619 <wayne900619@gmail.com>
2019-11-28 14:23:06 +0800

    complete reCAPTCHA

--- controller/reCAPTCHA.go
+++ controller/reCAPTCHA.go
@@ -10,13 +10,13 @@ import (
 	"../model"
 )
 
-const reCAPCTHAhost = "https://www.google.com/recaptcha/api/siteverify"
+const reCAPTCHAhost = "https://www.google.com/recaptcha/api/siteverify"
 const secret = "6LeVCsQUAAAAAGBAV-vfrR0-oCg7YjvzmEzZLqfw"
 
 func vailUser(token string, userIP string) (success bool, err error) {
 	success = false
-	var recapcthaRES model.RecapcthaResponse
-	response, err := http.PostForm(reCAPCTHAhost, url.Values{"secret": {secret}, "remoteip": {userIP}, "response": {token}})
+	var recaptchaRES model.RecaptchaResponse
+	response, err := http.PostForm(reCAPTCHAhost, url.Values{"secret": {secret}, "remoteip": {userIP}, "response": {token}})
 	if err != nil {
 		log.Printf("POST err:%s\n", err)
 		return
@@ -27,11 +27,11 @@ func vailUser(token string, userIP string) (success bool, err error) {
 		log.Printf("READ body error:%s\n", err)
 		return
 	}
-	err = json.Unmarshal(body, &recapcthaRES)
+	err = json.Unmarshal(body, &recaptchaRES)
 	if err != nil {
 		log.Printf("Transfer json error:%s\n", err)
 		return
 	}
-	success = recapcthaRES.Success
+	success = recaptchaRES.Success
 	return
 }
