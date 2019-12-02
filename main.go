7e401ffcdac885648b0e03ca6487e0a565cdc21a
Xanonymous <trusaidlin@gmail.com>
2019-12-02 17:31:40 +0800

    ssl add ^_^

--- main.go
+++ main.go
@@ -23,6 +23,6 @@ func main() {
 	router.POST("/signin", controller.VerifiesUser)
 	router.POST("/signup", controller.CreateNewuser)
 	router.NoRoute(controller.Gettingtodolist)
-	router.Run(":80")
+	router.Run(":443")
 
 }
